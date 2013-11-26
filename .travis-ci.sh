d7c7e80dac89a1fcda6701a36b63dc4cdf165365
Mickaël Delahaye <mickael.delahaye@gmail.com>
2013-11-26 14:52:51 +0100

    Update Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,6 +12,12 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
+
+echo Ocaml version
+ocaml -version
+echo OPAM version
+opam -version
+
 opam init 
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
